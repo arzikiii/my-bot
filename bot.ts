2a7204b18e9f4b479db7b36ebaa75c39d7b46974
arzikiii <arzi1605@gmail.com>
2024-03-10 16:33:26 +0700

    gaskan abangkuhhhhh

--- bot.ts
+++ bot.ts
@@ -1,12 +1,64 @@
-import { Bot } from "grammy";
-
+import { Bot, InlineKeyboard, InputFile, session } from "grammy";
+import { Menu } from "@grammyjs/menu";
 import dotenv from "dotenv";
+import { Input } from "telegraf";
+import { fmtCaption } from "telegraf/typings/core/helpers/util";
 dotenv.config();
 
 const bot = new Bot(process.env.TELE_BOT_TOKEN as string);
+console.log("bot started!");
+// const keyboard = new InlineKeyboard.url("Super Shy!");
 
-bot.command("start", (ctx) => ctx.reply("Hello, MoJune here!"));
+const menu = new Menu("my-menu-identifier")
+  .text("Greet Me!", async (ctx) => {
+    console.log(ctx);
+    ctx.reply(`Hi ${ctx.update?.callback_query?.from?.first_name}!`, {
+      reply_parameters: { message_id: ctx.msg!.message_id },
+      message_thread_id: 348,
+    });
+  })
+  .row()
+  .url("Super Shy?", "https://www.youtube.com/watch?v=ArmDp-zijuc&pp=ygUJc3VwZXIgc2h5")
+  .text("Who are you?", (ctx) =>
+    ctx.reply(`My name is ${ctx.me?.first_name}`, {
+      reply_parameters: { message_id: ctx.msg!.message_id },
+      message_thread_id: 348,
+    })
+  )
+  .text("Can i See you?", (ctx) => {
+    ctx.replyWithPhoto(new InputFile("hanni.jpg"), { message_thread_id: 348, caption: "Ofcourse honey!", reply_parameters: { message_id: ctx.msg!.message_id } });
+  });
+bot.use(menu);
 
-bot.on("message", (ctx) => ctx.reply("Got another message"));
+bot.command("start", (ctx) => {
+  if (Number(ctx.message?.message_thread_id) === 348) {
+    ctx.reply("Hello World!", {
+      reply_parameters: { message_id: ctx.msg!.message_id },
+    });
+  }
+});
 
-bot.start();
+bot.command("greet", (ctx) => {
+  ctx.reply("Halo Ayang, Check this out!", {
+    reply_parameters: { message_id: ctx.msg!.message_id },
+    reply_markup: menu,
+  });
+});
+
+bot.on("message").hears(/(spontan)/g, async (ctx) => {
+  if (ctx.message?.chat?.type === "supergroup")
+    ctx.reply("uhuyyy", {
+      reply_parameters: { message_id: ctx.msg!.message_id },
+      message_thread_id: 348,
+    });
+});
+
+bot.on("message").hears(/(gacor)/g, async (ctx) => {
+  if (ctx.message?.chat?.type === "supergroup")
+    ctx.reply("Menyala abangkuhhhh 🔥👊🏼", {
+      reply_parameters: { message_id: ctx.msg!.message_id },
+      message_thread_id: 348,
+    });
+});
+
+bot.start({ drop_pending_updates: true });
---
+++ unused.js
@@ -0,0 +1,34 @@
+"use strict";
+// const menu = new Menu("my-menu-identifier")
+//   .text("A", (ctx) => ctx.reply("You pressed A!"))
+//   .row()
+//   .text("B", (ctx) => ctx.reply("You pressed B!"));
+// // Make it interactive.
+// bot.use(menu);
+// const main = new Menu("root-menu")
+//   .text("Welcome", (ctx) => ctx.reply("Hi!"))
+//   .row()
+//   .submenu("Credits", "credits-menu");
+// const settings = new Menu("credits-menu").text("Show Credits", (ctx) => ctx.reply("Powered by grammY")).back("Go Back");
+// main.register(settings);
+// // main.register(settings, "back-from-settings-menu");
+// bot.use(main);
+// bot.on(["message", "edited_message"], async (ctx) => {
+//   const spontan = ctx.message?.text?.includes("spontan");
+//   if (spontan) {
+//     ctx.reply("uhuyyy", {
+//       reply_parameters: { message_id: ctx.msg!.message_id },
+//       message_thread_id: 348,
+//     });
+//   }
+//   if (Number(ctx.message?.from.id) === 2129027862) {
+//     ctx.reply("Halo ayang", {
+//       reply_parameters: { message_id: ctx.msg!.message_id },
+//       message_thread_id: ctx.update.message?.message_thread_id,
+//     });
+//     console.log(JSON.stringify(ctx, null, 2));
+//     console.log("log ctx");
+//   }
+//   console.log(JSON.stringify(ctx.update, null, 2));
+//   //   await bot.api.sendMessage(2129027862, "Bot responded to message");
+// });
